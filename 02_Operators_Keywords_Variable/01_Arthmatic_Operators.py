1b37d9088ce1480d6a46504ccc12e60990256c11
Talha Shaikh <Talha369852@gmail.com>
2026-08-27 21:41:10 +0500

    add arthmatic operators

---
+++ 02_Operators_Keywords_Variable/01_Arthmatic_Operators.py
@@ -0,0 +1,17 @@
+# +	Addition	5 + 2 = 7
+# -	Subtraction	5 - 2 = 3
+# *	Multiplication	5 * 2 = 10
+# /	Division (float)	5 / 2 = 2.5
+# //	Floor Division	5 // 2 = 2 (removes decimal part)
+# %	Modulus (remainder)	5 % 2 = 1
+# **	Exponentiation	5 ** 2 = 25
+
+a: int = 10
+b: int = 3
+print("a + b  = ", a + b)   # 13 Addition
+print("a - b  = ", a - b)   # 7 Subtraction
+print("a * b  = ", a * b)   # 30 Multiplication
+print("a / b  = ", a / b)   # 3.3333333333333335
+print("a // b = ", a // b)  # 3 Floor Division
+print("a % b  = ", a % b)   # 1 Modulus (remainder)
+print("a ** b  = ", a ** b)  # 1000 Exponentiation (10 * 10 * 10)
