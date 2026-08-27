ccc7dd8e46252d776de0a1140c1f9ad2b47326b9
Talha Shaikh <Talha369852@gmail.com>
2026-08-27 21:57:39 +0500

    add membership operators

---
+++ 02_Operators_Keywords_Variable/06_Membership_Operators.py
@@ -0,0 +1,22 @@
+# 6. Membership Operators
+# Used to check if a value is in a sequence (list, tuple, set, dictionary, etc.).
+
+
+# Operator	Name	                                            Example
+# in	    Returns True if value is in the sequence	        x in list
+# not in	Returns True if value is NOT in the sequence	    x not in list
+
+
+my_list: list = [1, 2, 3, 4, 5]
+
+print("my_list           = ", my_list)            # [1, 2, 3, 4, 5]
+print("3 in my_list      = ", 3 in my_list)       # True
+print("10 not in my_list = ", 10 not in my_list)  # True
+
+print("\n-----\n")
+
+my_string: str = "Operation Badar"
+
+print("my_string                 = ", my_string)                # Operation Badar
+print("'O' in my_string          = ", 'O' in my_string)         # True
+print("'Hello' not in my_string  = ", 'Hello' not in my_string) # True
